05b750295244782cad3d0bf8ff3416bff0e4ddeb
Dusch4593 <brandondusch@gmail.com>
2020-07-24 18:16:07 -0400

    began action setup in collections_controller

--- backend/app/controllers/collections_controller.rb
+++ backend/app/controllers/collections_controller.rb
@@ -1,2 +1,28 @@
 class CollectionsController < ApplicationController
+  before_action :set_collection, only: [:show, :update, :destroy]
+  # GET /collections
+  def index
+    @collections = Collection.all
+    render json: @collections
+  end
+
+  # GET /collections/:id
+  def show
+    render json: @collection
+  end
+
+  # POST /collections
+  def create
+
+  end
+
+  # PATCH/ PUT collections/:id
+
+  # DELETE collections/:id
+
+  private
+
+  def set_collection
+    @collection = Collection.find_by(id: params[:id])
+  end
 end
