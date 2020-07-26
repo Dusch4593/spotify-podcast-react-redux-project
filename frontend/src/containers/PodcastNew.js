60b3758e5f0e22e5039e90b0c53916e09d2660e0
Dusch4593 <brandondusch@gmail.com>
2020-07-26 16:06:40 -0400

    refactored new podcast form

--- frontend/src/containers/PodcastNew.js
+++ frontend/src/containers/PodcastNew.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react'
+import { addPodcast } from '../actions/index'
+import { connect } from 'react-redux'
 
 class PodcastNew extends Component {
   state = {
-    name: ''
+    name: '',
+    hosts: '',
+    genres: '',
+    link: ''
   }
 
 
   handleChange = e => {
     this.setState({
-      name: e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
 
   handleSubmit = e => {
     e.preventDefault()
-    alert("form submitted!")
+    this.props.addPodcast(this.state)
+    document.getElementById('new-podcast-form').reset()
+    this.props.history.push('/podcasts')
   }
 
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Create New Collection</h3>
-        <label>Name</label>:
-        <input type="text" onChange={this.handleChange} value={this.state.name} />
-        <br />
+      <form id="new-podcast-form" onSubmit={this.handleSubmit}>
+        <h3>Add New Podcast</h3>
+        <label>Name</label> <br />
+        <input type="text" onChange={this.handleChange} value={this.state.name} name="name"/>
+        <br /> <br />
+        
+        <label>Hosts/ Publisher (if applicable)</label> <br />
+        <input type="text" onChange={this.handleChange} value={this.state.hosts} name="hosts"/>
+        <br /> <br />
+
+        <label>Genres (comma-separated list)</label> <br />
+        <input type="text" onChange={this.handleChange} value={this.state.genres} name="genres"/>
+        <br /> <br />
+
+        <label>Link (such as <a href="https://open.spotify.com/show/3IM0lmZxpFAY7CwMuv9H4g">https://open.spotify.com/show/3IM0lmZxpFAY7CwMuv9H4g</a>)</label> <br />
+        <input type="text" onChange={this.handleChange} value={this.state.link} name="link"/>
+        <br /> <br />
+
         <input type="submit" value="Create New Collection" />
       </form>
     )
   }
 }
 
-export default PodcastNew
+export default connect(null, { addPodcast })(PodcastNew)
