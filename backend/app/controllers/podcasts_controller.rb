68a21702ae0e8bf49335c0a83fd5ac9b54960c3e
Dusch4593 <brandondusch@gmail.com>
2020-07-26 05:20:39 -0400

    added index and show actions to podcasts_controller

--- backend/app/controllers/podcasts_controller.rb
+++ backend/app/controllers/podcasts_controller.rb
@@ -1,2 +1,18 @@
 class PodcastsController < ApplicationController
+  before_action :set_podcast, only: [:show, :update, :destroy]
+  def index
+    @podcasts = Collection.find_by(params[:collection_id]).podcasts
+
+    render json: @podcasts
+  end
+
+  def show
+    render json: @podcast
+  end
+
+
+  private
+  def set_podcast
+    @podcast = Podcast.find_by(params[:id])
+  end
 end
