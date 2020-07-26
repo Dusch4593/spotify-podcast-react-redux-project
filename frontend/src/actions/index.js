2d493b5ac4c5aee73536d85add4b1728f24b9595
Dusch4593 <brandondusch@gmail.com>
2020-07-26 17:15:51 -0400

    refactored the action creators to make async fetch requests to the backend

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,17 +1,46 @@
 // fetch (GET) the current list of podcasts in the Rails backend
 // => GET localhost:3001/podcasts
 export const fetchPodcasts = () => {
-  return {type: "GET_PODCASTS"}
+  return dispatch => {
+    return fetch(`http://localhost:3001/podcasts`)
+      .then(res => res.json())
+      .then(podcasts => {
+        dispatch({type: "GET_PODCASTS", payload: podcasts})
+      })
+  }
 }
 
 // add (POST) a new podcast to a the DB
 // => POST localhost:3001/podcasts
 export const addPodcast = podcast => {
-  return {type: "ADD_PODCAST", payload: podcast}
+  return dispatch => {
+    return fetch(`http://localhost:3001/podcasts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({podcast: podcast})
+    })
+      .then(res => res.json())
+      .then(podcast => {
+        dispatch({type: "ADD_PODCAST", payload: podcast})
+      })
+  }
 }
 
 // remove (DELETE) a specific podcast (:id) from both the frontend and the backend
 // => DELETE localhost:3001/podcasts/:id
 export const deletePodcast = podcastId => {
-  return {type: "REMOVE_PODCAST", payload: podcastId}
+  return dispatch => {
+    return fetch(`http://localhost:3001/podcasts/${podcastId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(podcast => {
+        dispatch({type: "REMOVE_PODCAST", payload: podcastId})
+      })
+  }
 }
