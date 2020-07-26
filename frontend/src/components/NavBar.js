b31102a993fafe90a53488cece21f13f2c85c9ac
Dusch4593 <brandondusch@gmail.com>
2020-07-26 15:20:52 -0400

    created two stateless components: AddButton and NavBar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="nav-links">
+          <li>< NavLink to="/podcasts/new">Add Podcast</ NavLink ></li>
+          <li>< NavLink to="/podcasts">See All Podcasts</ NavLink ></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default NavBar
