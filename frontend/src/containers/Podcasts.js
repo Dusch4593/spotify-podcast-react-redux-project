728a1fe1f6ba6050090befa2421d1f2e50b920c4
Dusch4593 <brandondusch@gmail.com>
2020-07-26 16:07:31 -0400

    added dispatched actions to props

--- frontend/src/containers/Podcasts.js
+++ frontend/src/containers/Podcasts.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import PodcastCard from '../components/PodcastCard'
 import { connect } from 'react-redux'
+import AddButton from '../components/AddButton'
+import DeleteButton from '../components/DeleteButton'
+import { fetchPodcasts, addPodcast, deletePodcast } from '../actions/index'
 
 class Podcasts extends Component {
+
+  componentDidMount() {
+    this.props.fetchPodcasts()
+  }
+
   render() {
     return(
       <div>
-
+        < AddButton name="Add Podcast" addPodcast={this.props.addPodcast}/>
         {this.props.podcasts.map((p, id) => (
-          < PodcastCard key={id} podcast={p} />
+          <div>
+            < PodcastCard key={id} podcast={p} />
+            < DeleteButton key={id} name="Delete Podcast" podcast_id={id} deletePodcast={this.props.deletePodcast}/>
+          </div>
         ))}
       </div>
     )
@@ -21,4 +32,12 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Podcasts)
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchPodcasts: () => dispatch(fetchPodcasts()),
+    addPodcast: podcast => dispatch(addPodcast(podcast)),
+    deletePodcast: podcastId => dispatch(deletePodcast(podcastId))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Podcasts)
