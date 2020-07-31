32ea119204848856d9da3b57db0b8fdc087189ee
Dusch4593 <brandondusch@gmail.com>
2020-07-31 13:44:18 -0400

    light styling on home component

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div id="home-container">
         <h1>Welcome To My Podcasts App!</h1>
         <a className="home-page-link" href="/podcasts">View Podcasts</a> <br />
         <a className="home-page-link" href="/podcasts/new">Add New Podcast</a> <br />
